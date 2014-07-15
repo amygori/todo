2cd9c5477d0fd73d7474d0f9391d14dcae2b484c
amygori <amydgori@gmail.com>
2014-07-15 09:50:33 -0400

    minor changes to get input view working with api endpoint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-(function ($) {
+//(function ($) {
 //instantiate collection
 	var tasks = new Tasks();
 
@@ -10,4 +10,4 @@
 			return false; 
 		});
 	});
-})(jQuery);
+//})(jQuery);
--- app/scripts/views.js
+++ app/scripts/views.js
@@ -3,41 +3,58 @@
 var ENTER_KEY = 13;
 var ListView;
 var AppView;
-
-(function ($) {
-	'use strict';
+//instantiate collection
+var tasks = new Tasks();
 
 //view for the individual tasks 
 	ListView = Backbone.View.extend({
 		tagName: 'li',
 		model: new Task, //the model
+		events: {
+			'click .edit' : 'edit',
+			'click .delete' : 'delete',
+			'blur .title' : 'close',
+			'keypress .title' : 'enterSubmit'
+		},
 		initialize: function(){
 			this.template = Handlebars.compile($('#template').html()); 
 			//this.listenTo(this.model, 'change', this.render);
 			//	this.listenTo(this.model, 'destroy', this.remove);
+		},
+		edit: function(e){
+			e.preventDefault();
+			this.$('.title').attr('editable', true).focus();
+		},
+		close: function(e){
+			var title = this.$('.title').text();
+			this.model.set('title', title);
+			this.$('.title').removeAttr('editable');
+		},
+		delete: function(e){
+			e.preventDefault();
+			tasks.remove(this.model);
+		},
+		enterSubmit: function(e){
+			if(e.keycode === ENTER_KEY){
+      			this.close();
+      			return this;
+      		}
 		},
 	    render: function(){
 	        var rendered = this.template(this.model.toJSON());
 	        this.$el.html(rendered); //should this be .html instead of .append?
 	        this.$el.toggleClass('done', this.model.get('done'));
 	        return this; 
-		},		
+		}		
 	});
 
 	
 //view for the app
 	AppView = Backbone.View.extend({
-		model: Tasks, //the collection
+		model: tasks, //the collection NB: is this the right reference?
 		el: $('#taskList'),
 		initialize: function(){
         	this.model.on('add', this.render, this);
-
-
-        	/*this.input = this.$('#newTask');
-        	tasks.on('add', this.addAll, this);
-        	tasks.on('reset', this.addAll, this);
-        	tasks.fetch();
-        	this.listenTo(tasks, 'add', this.addTask);*/
 		},
 		render: function(){
 			var self = this;
@@ -47,39 +64,8 @@ var AppView;
 			});
 			return this;
 		},
-		
-		/*events: {
-        	'keypress #newTask': 'createNewTask'
-      	},	
-      	
-      	
-
-
-
-      	createNewTask: function(e){
-      		if(e.which === ENTER_KEY){
-      			this.close();
-      			return this;
-      		}
-        	tasks.create(this.newAttributes());
-        	this.input.val(''); 
-      	},
-      	addTask: function (task) {
-      		var view = new ListView({model: task});
-      		this.$list.append(view.render().el);
-      	},
-      	addAll: function(){
-        	this.$('#todo-list').html(''); 
-        	tasks.each(this.addOne, this);
-      	},
-      	newAttributes: function(){
-        	return {
-          		title: this.input.val().trim(),
-          		done: false
-        	}
-      	}*/
 	});
-})(jQuery);
+
 
 
 var appview = new AppView();
